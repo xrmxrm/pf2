2409ee66850c7840c0458781f5d61d7c290b345c
xrmxrm <xrm@gmail.com>
2022-03-06 18:48:36 -0800

    Add home page

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from pfolio import views
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
@@ -20,5 +21,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
